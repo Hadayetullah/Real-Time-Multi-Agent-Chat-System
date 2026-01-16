301e1db5b3bafa4c46d3c1ae6b48f2eee3446e58
Hadayetullah <hadayetullah30@gmail.com>
2026-01-16 22:24:52 +0600

    Initialize frontend (Next.Js)

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Project 2 – Real-Time Multi-Agent Chat System (Frontend NextJS)</h1>
+    </div>
+  );
+}
